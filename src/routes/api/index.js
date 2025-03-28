0a821f200d1ceb6b9fd998cdf40580d7903b7a5f
Yuelin Wen <wenyuelinca@gmail.com>
2025-03-28 14:43:21 -0400

    audit

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -11,9 +11,6 @@ const rawBody = () =>
     inflate: true,
     limit: "5mb",
     type: (req) => {
-      // See if we can parse this content type. If we can, `req.body` will be
-      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
-      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
       const { type } = contentType.parse(req);
       return File.isSupportedType(type);
     },
